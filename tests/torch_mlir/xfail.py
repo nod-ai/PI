482fb3645475b2973f2dcf3bc947462354c94542
max <maksim.levental@gmail.com>
2023-02-03 16:57:03 -0600

    add bucketize to xfail

--- tests/torch_mlir/xfail.py
+++ tests/torch_mlir/xfail.py
@@ -1,4 +1,5 @@
 PI_XFAIL_SET = {
+
     # view op (Which doesn't pass regular torch-mlir either???)
     "BernoulliFloatModule_basic",
     "ElementwiseFlattenBroadcastModule_basic",
@@ -33,6 +34,13 @@ PI_XFAIL_SET = {
     "Fill_TensorFloat64WithInt64_basic",
 
     # eager/lazy materialization (24) TypeError: object of type 'Torch_Value' has no len()
+    "BucketizeTensorFloatModule_basic",
+    "BucketizeTensorModule_basic",
+    "BucketizeTensorOutInt32RightModule_basic",
+    "BucketizeTensorFloatModule_basic",
+    "BucketizeTensorStaticModule_basic",
+    "BucketizeTensorStaticFloatModule_basic",
+
     "EmbeddingModule1DIndices_basic",
     "EmbeddingModuleF16_basic",
     "EmbeddingModuleI32Static_basic",
