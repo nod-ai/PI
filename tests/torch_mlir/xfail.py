fe8d0f2b9216c0663d8eb5016b7757e2e66eefc4
Arham Khan <arhamkhan@Arhams-MacBook-Pro.local>
2023-06-16 16:19:35 -0500

    add trivial ir fails 'f64+truncf'

--- tests/torch_mlir/xfail.py
+++ tests/torch_mlir/xfail.py
@@ -16,4 +16,13 @@ CRASHING = {
     "UniformModule_basic",
 }
 
-PI_XFAIL_SET = {"ElementwiseGeFloatScalarModule_basic"}
+PI_XFAIL_SET = {"ElementwiseGeFloatScalarModule_basic", "ArangeStartNegativeStepFloatModule_basic",
+                "ArangeStartStepFloatModule_basic", "BaddbmmWithAlphaBetaModule_basic", "ElementwiseLeakyReluModule_basic",
+                "ElementwiseLeakyReluStaticModule_basic", "ElementwiseLeFloatScalarModule_basic", "ElementwiseGtFloatScalarModule_basic",
+                "ElementwiseSubScalarFloatModule_basic",
+                "HardtanhBackward_basic",
+                "LeakyReluBackwardModule_basic",
+                "LeakyReluBackwardStaticModule_basic",
+                "Threshold3dFloatModule_basic",
+                "ThresholdBackward3dFloatModule_basic"
+                }
