917189fae3bd29ae51d23a49a02b23d85f079675
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2023-07-06 09:12:09 -0700

    implemented cdef for permute()

--- scripts/generate_stuff/generate_torch_mlir_bindings_from_torch_json.py
+++ scripts/generate_stuff/generate_torch_mlir_bindings_from_torch_json.py
@@ -63,6 +63,7 @@ SKIP_TENSOR_BINDS = {
     "chunk(self, chunks: _int, dim: _int=0) -> List[Tensor]",
     "__getitem__(self, indices: Union[None, _int, slice, Tensor, List, Tuple]) -> Tensor",
     "double(self) -> Tensor",
+    "@overload permute(self, dims: _size) -> Tensor",
 }
 
 TORCH_OPS_IMPL_CPP = "TorchOps.impls.cpp"
