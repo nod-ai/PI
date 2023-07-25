58f80d8d38a679197f5d785bb3debe8b051d94cb
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2023-07-25 14:06:29 -0700

    added comma for reformatting

--- tests/unit/test_list_int_bindings.py
+++ tests/unit/test_list_int_bindings.py
@@ -18,35 +18,35 @@ class TestListIntBindings:
             l = ops.max_pool2d(t, 3, 1)
             check_correct(
                 " %4 = torch.aten.max_pool2d %arg0, %1, %2, %3, %0, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor",
-                l.owner
+                l.owner,
             )
 
             t = pi.rand(1, 3, 32, 32)
             l = ops.max_pool2d(t, (3, 3), 1)
             check_correct(
                 " %4 = torch.aten.max_pool2d %arg0, %1, %2, %3, %0, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor",
-                l.owner
+                l.owner,
             )
 
             t = pi.rand(1, 3, 32, 32)
             l = ops.max_pool2d(t, 3, (2, 2))
             check_correct(
                 " %4 = torch.aten.max_pool2d %arg0, %1, %2, %3, %0, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor",
-                l.owner
+                l.owner,
             )
 
             t = pi.rand(1, 3, 32, 32)
             l = ops.max_pool2d(t, 3, 1, 2, 3)
             check_correct(
                 " %4 = torch.aten.max_pool2d %arg0, %1, %2, %3, %0, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor",
-                l.owner
+                l.owner,
             )
 
             t = pi.rand(1, 3, 32, 32)
             l = ops.max_pool2d(t, 3, 1, [2, 2], 3)
             check_correct(
                 " %4 = torch.aten.max_pool2d %arg0, %1, %2, %3, %0, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor",
-                l.owner
+                l.owner,
             )
 
     def test_max_pool2d_with_indices(self):
@@ -55,26 +55,26 @@ class TestListIntBindings:
             l, s = ops.max_pool2d_with_indices(t, 3)
             check_correct(
                 "%result0, %result1 = torch.aten.max_pool2d_with_indices %arg0, %0, %1, %2, %1, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor, !torch.tensor",
-                l.owner
+                l.owner,
             )
 
             t = pi.rand(1, 3, 32, 32)
             l, s = ops.max_pool2d_with_indices(t, 3, (1, 1), 2)
             check_correct(
                 "%result0, %result1 = torch.aten.max_pool2d_with_indices %arg0, %0, %1, %2, %1, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor, !torch.tensor",
-                l.owner
+                l.owner,
             )
 
             t = pi.rand(1, 3, 32, 32)
             l, s = ops.max_pool2d_with_indices(t, 3, (1, 1), 2, [4, 4])
             check_correct(
                 "%result0, %result1 = torch.aten.max_pool2d_with_indices %arg0, %0, %1, %2, %1, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor, !torch.tensor",
-                l.owner
+                l.owner,
             )
 
             t = pi.rand(1, 3, 32, 32)
             l, s = ops.max_pool2d_with_indices(t, 3, 1, 2, 4)
             check_correct(
                 "%result0, %result1 = torch.aten.max_pool2d_with_indices %arg0, %0, %1, %2, %1, %false : !torch.tensor<[1,3,32,32],f64>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.list<int>, !torch.bool -> !torch.tensor, !torch.tensor",
-                l.owner
+                l.owner,
             )
